4a2bcd9402936130fd1167d156a04d6d548cde18
antonio marcos <marcosgeo@yahoo.com>
2023-01-31 07:14:21 -0300

    List books

--- src/app.py
+++ src/app.py
@@ -2,10 +2,16 @@
 
 from bumbo.api import API
 
+from .storage import BookStorage
+
+
 app = API(templates_dir="src/templates", static_dir="src/static")
+book_storage = BookStorage()
+book_storage.create(name="7 habits of highly effective people", author="Stephen Covey")
 
 
 @app.route("/", allowed_methods=["get"])
 def index(req, resp):
-    resp.html = app.template("index.html")
+    books = book_storage.all()
+    resp.html = app.template("index.html", context={"books": books})
 
---
+++ src/models.py
@@ -0,0 +1,12 @@
+# models.py
+
+from typing import NamedTuple
+
+
+class Book(NamedTuple):
+    id: int
+    name: int
+    author: str
+
+
+
